64e7f16699fc0c41e95a42a6f4e7ed4101b92810
Jakealope <jake.anderson486@gmail.com>
2015-02-10 13:40:03 -0800

    server is set up to listen and send a respose when ping(ed)

--- echo_server.py
+++ echo_server.py
@@ -1,14 +1,20 @@
 import socket
 
-server_socket = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_TCP)
+try:
+    while True:
+        server_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+            socket.IPPROTO_TCP)
 
-server_socket
-address = ('127.0.0.1', 50000)
-server_socket.bind(address)
-server_socket.listen(1)
-connection, client_address = server_socket.accept()
-connection.recv(16)
-conn, addr = server_socket.accept()
+        address = ('127.0.0.1', 50000)
+        server_socket.bind(address)
+        server_socket.listen(1)
+        connection, client_address = server_socket.accept()
+        connection.recv(32)
+        connection.sendall("message received")
+        connection.shutdown(socket.SHUT_WR)
+        connection.close()
+
+except KeyboardInterrupt:
+    server_socket.close()
