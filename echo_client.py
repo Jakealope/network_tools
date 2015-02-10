ce05575bdfe756bb1b8542c4db556eeb6ce957f5
Jakealope <jake.anderson486@gmail.com>
2015-02-10 13:53:53 -0800

    updated connections so client now talks to server

--- echo_client.py
+++ echo_client.py
@@ -1,6 +1,21 @@
 import socket
-client_socket = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_IP)
-client_socket.connect(('127.0.0.1', 50000))
+import sys
+
+
+def clientmessage(msg):
+
+    client_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    client_socket.connect(('127.0.0.1', 50000))
+    client_socket.sendall(msg)
+    client_socket.shutdown(socket.SHUT_WR)
+    buffersize = 32
+    echomsg = client_socket.recv(buffersize)
+    print echomsg
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    clientmessage(sys.argv[1])
