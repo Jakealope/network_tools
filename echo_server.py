fbfa3bc8cc3c486948bda5302a0844edbff48e90
Jakealope <jake.anderson486@gmail.com>
2015-02-10 17:59:23 -0800

    added a loop to let it recieve the full message

--- echo_server.py
+++ echo_server.py
@@ -1,19 +1,25 @@
 import socket
 
 try:
-    while True:
-        server_socket = socket.socket(
-            socket.AF_INET,
-            socket.SOCK_STREAM,
-            socket.IPPROTO_TCP)
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_TCP)
 
-        address = ('127.0.0.1', 50000)
-        server_socket.bind(address)
-        server_socket.listen(1)
+    address = ('127.0.0.1', 50000)
+    server_socket.bind(address)
+    server_socket.listen(1)
+    while True:
         connection, client_address = server_socket.accept()
-        buffersize = 16
-        echomsg = connection.recv(buffersize)
-        connection.sendall(echomsg)
+        buffersize = 32
+        response = ''
+        done = False
+        while not done:
+            msg_part = connection.recv(buffersize)
+            if len(msg_part) < buffersize:
+                done = True
+            response += msg_part
+        connection.sendall(response)
         connection.shutdown(socket.SHUT_WR)
         connection.close()
 
