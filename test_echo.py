fb8404dcb4b7ba8d3da6e470a4e143cb80c9fb4c
Jakealope <jake.anderson486@gmail.com>
2015-02-10 18:38:18 -0800

    added tests to prove more parts of working server ping

--- test_echo.py
+++ test_echo.py
@@ -2,5 +2,13 @@ from echo_client import clientmessage
 import pytest
 
 
-def test_unicode():
+def test_ping():
     assert clientmessage("this is a test") == "this is a test"
+
+
+def test_big():
+    assert clientmessage("this is a string that is much longer than the 32 bites that it has to force it to loop") == "this is a string that is much longer than the 32 bites that it has to force it to loop"
+
+
+def test_empty():
+    assert clientmessage("") == ""
