f097edc6b1ad7b3a192d4d127a9cec0346571ce5
Jakealope <jake.anderson486@gmail.com>
2015-02-10 17:59:47 -0800

    added a loop to let it send the full message

--- echo_client.py
+++ echo_client.py
@@ -11,10 +11,16 @@ def clientmessage(msg):
     client_socket.connect(('127.0.0.1', 50000))
     client_socket.sendall(msg)
     client_socket.shutdown(socket.SHUT_WR)
-    buffersize = 16
-    echomsg = client_socket.recv(buffersize)
-    print echomsg
-    client_socket.close()
+    buffsize = 32
+    response = ''
+    done = False
+    while not done:
+        msg_part = client_socket.recv(buffsize)
+        if len(msg_part) < buffsize:
+            done = True
+            client_socket.close()
+        response += msg_part
+    return response
 
 
 if __name__ == '__main__':
