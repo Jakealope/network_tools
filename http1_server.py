37ffe26346951245339990070f4d1624f1401422
Jakealope <jake.anderson486@gmail.com>
2015-02-16 18:39:28 -0800

    added comments to find bug spots, passing

--- http1_server.py
+++ http1_server.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#!/usr/bin/env python
 import socket
 import email.utils
 
@@ -7,16 +6,16 @@ import email.utils
 def response_ok(request):
     response_code = "HTTP/1.1 200 OK"
     date = "Date: {}".format(email.utils.formatdate(usegmt=True))
-    headers = "Content-Type Text/HTML"
+    headers = "Content-Type: Text/HTML"
     response = "{}\r\n{}\r\n{}\r\n\r\n".format(response_code, date, headers)
-    return response.encode('utf-8')
+    return response
 
 
 def response_error(code, reason):
     response_code = "HTTP/1.1 {} {}".format(code, reason)
     date = "Date: {}".format(email.utils.formatdate(usegmt=True))
-    headers = "Content-Type:Text/HTML"
-    response = "{}\r\n{}\r\n{}\r\n\r\n".format(response_code, date, headers)
+    headers = "Content-Type: Text/HTML"
+    response = u"{}\r\n{}\r\n{}\r\n\r\n".format(response_code, date, headers)
     return response.encode('utf-8')
 
 
@@ -24,36 +23,45 @@ def parse_request(request):
     request = request.split('\r\n')
     first_line = request[0].split(' ')
     if first_line[0] != "GET":
-        return response_error(405, "Request not allowed")
-    if first_line[2] != "HTTP/1.1":
-        return response_error(505, "Invalid protocol")
-    return response_ok(first_line[1])
+        return response_error(405, "Method Not Allowed")
+    elif first_line[2] != "HTTP/1.1":
+        return response_error(505, "HTTP Version Not Supported")
+    else:
+        return response_ok(first_line[1])
 
-try:
-    server_socket = socket.socket(
-        socket.AF_INET,
-        socket.SOCK_STREAM,
-        socket.IPPROTO_TCP)
 
-    address = ('127.0.0.1', 50000)
-    server_socket.bind(address)
-    server_socket.listen(1)
-    while True:
-        connection, client_address = server_socket.accept()
-        buffersize = 4096
-        response = ''
-        done = False
-        while not done:
-            msg_part = connection.recv(buffersize)
-            if len(msg_part) < buffersize:
-                done = True
-            response += msg_part
+def server():
+    try:
+        server_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+            socket.IPPROTO_TCP)
+        server_socket.bind(('127.0.0.1', 50000))
+        server_socket.listen(1)
+        while True:
+            connection, client_address = server_socket.accept()
+            buffsize = 4096
+            req = ''
+            done = False
+            while not done:
+                msg = connection.recv(buffsize)
+                if len(msg) < buffsize:
+                    done = True
+                req += msg
 
-            response = parse_request(response)
+            # send full request to parser, prints request and response
+            response_msg = parse_request(req)
+            print "REQUEST: {}".format(req)
+            print "RESPONSE: \n{}\n--end--".format(response_msg)
+            connection.sendall(response_msg)
 
-        connection.sendall(response)
-        connection.shutdown(socket.SHUT_WR)
-        connection.close()
+            # shutdown socket to writing after sending echo message
+            connection.shutdown(socket.SHUT_WR)
+            connection.close()
+    except KeyboardInterrupt:
+        # close socket completely on interupt
+        server_socket.close()
 
-except KeyboardInterrupt:
-    server_socket.close()
+
+if __name__ == '__main__':
+    server()
